1c986df54083060ac3fb1da306ba11a4c7cfbbc9
Kauã Rodrigues <kaua.rodrigues2620@gmail.com>
2024-01-26 17:17:51 -0300

    Revisão

--- React Native/revisaoProps/App.js
+++ React Native/revisaoProps/App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Person from './src/components/Person';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView>
+      <Person name={'Kauã'} age={18}/>
+      <Person name={'Carlos'} age={37}/>
+      <Person name={'Edu'} age={38}/>
       <StatusBar style="auto" />
-    </View>
+      
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- React Native/revisaoProps/src/components/Person.js
+++ React Native/revisaoProps/src/components/Person.js
@@ -1,7 +1,24 @@
-const { View } = require("react-native")
+import { StyleSheet, Text, View } from "react-native";
 
-const Person = ({name, age}) =>{
-    return(
-        <View></View>
+const Person = ({ name, age }) => {
+    return (
+        <View style={styles.container}>
+            <Text>Nome: {name}</Text>
+            <Text>Idade: {age}</Text>
+        </View>
     )
-}
+
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#c1c1c1',
+        padding: 10,
+        margin: 10,
+        borderRadius: 5,
+    }
+})
+
+
+
+export default Person;
